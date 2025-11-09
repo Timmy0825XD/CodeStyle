0f4b04d2de7248d0ebbf049d11410c1a38797787
Timmy Dev <joshephmartinez2007@gmail.com>
2025-11-09 15:04:36 -0500

    Update of the classes responsible for the USERS module

--- DAL/Implementaciones/UsuarioDAO.cs
+++ DAL/Implementaciones/UsuarioDAO.cs
@@ -4,6 +4,7 @@ using Dapper;
 using ENTITY.Usuarios;
 using ENTITY.Utilidades;
 using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -21,32 +22,53 @@ namespace DAL.Implementaciones
             _connectionString = connectionString;
         }
 
+        // ========================================
+        // 1. LOGIN
+        // ========================================
         public async Task<Response<LoginResponseDTO>> Login(LoginRequestDTO loginRequest)
         {
             try
             {
                 using (var connection = new OracleConnection(_connectionString))
                 {
-                    var parameters = new OracleDynamicParameters();
-                    parameters.Add("p_correo", loginRequest.Correo, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_contrasena", loginRequest.Contrasena, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("v_cursor", dbType: OracleDbType.RefCursor, direction: ParameterDirection.ReturnValue);
+                    await connection.OpenAsync();
 
-                    var result = await connection.QueryAsync<LoginResponseDTO>(
-                        "pkg_usuarios.login_usuario",
-                        parameters,
-                        commandType: CommandType.StoredProcedure
-                    );
-
-                    var usuario = result.FirstOrDefault();
-
-                    if (usuario != null)
+                    using (var command = connection.CreateCommand())
                     {
-                        return Response<LoginResponseDTO>.Done("Login exitoso", usuario);
-                    }
-                    else
-                    {
-                        return Response<LoginResponseDTO>.Fail("Credenciales inválidas");
+                        command.CommandText = "pkg_usuarios.login_usuario";
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Parámetros de entrada
+                        command.Parameters.Add("p_correo", OracleDbType.Varchar2).Value = loginRequest.Correo;
+                        command.Parameters.Add("p_contrasena", OracleDbType.Varchar2).Value = loginRequest.Contrasena;
+
+                        // Parámetro de salida (cursor)
+                        var cursorParam = new OracleParameter("p_cursor", OracleDbType.RefCursor, ParameterDirection.Output);
+                        command.Parameters.Add(cursorParam);
+
+                        await command.ExecuteNonQueryAsync();
+
+                        // Leer el cursor
+                        using (var reader = ((OracleRefCursor)cursorParam.Value).GetDataReader())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                var usuario = new LoginResponseDTO
+                                {
+                                    IdUsuario = reader.GetInt32(reader.GetOrdinal("ID_USUARIO")),
+                                    NombreCompleto = $"{reader.GetString(reader.GetOrdinal("PRIMER_NOMBRE"))} {reader.GetString(reader.GetOrdinal("APELLIDO_PATERNO"))}",
+                                    IdRol = reader.GetInt32(reader.GetOrdinal("ID_ROL"))
+                                };
+
+                                Console.WriteLine($"Usuario logueado: {usuario.IdUsuario} - {usuario.NombreCompleto} - Rol: {usuario.IdRol}");
+
+                                return Response<LoginResponseDTO>.Done("Login exitoso", usuario);
+                            }
+                            else
+                            {
+                                return Response<LoginResponseDTO>.Fail("Credenciales inválidas");
+                            }
+                        }
                     }
                 }
             }
@@ -64,39 +86,52 @@ namespace DAL.Implementaciones
             }
         }
 
+        // ========================================
+        // 2. CREAR USUARIO
+        // ========================================
         public async Task<Response<int>> CrearUsuario(UsuarioDTO usuario)
         {
             try
             {
                 using (var connection = new OracleConnection(_connectionString))
                 {
-                    var parameters = new OracleDynamicParameters();
-                    parameters.Add("p_id_direccion", usuario.DireccionId, OracleDbType.Int32, ParameterDirection.Input);
-                    parameters.Add("p_cedula", usuario.Cedula, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_primer_nombre", usuario.PrimerNombre, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_segundo_nombre", usuario.SegundoNombre, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_apellido_paterno", usuario.ApellidoPaterno, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_apellido_materno", usuario.ApellidoMaterno, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_telefono_principal", usuario.TelefonoPrincipal, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_telefono_secundario", usuario.TelefonoSecundario, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_correo", usuario.Correo, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_contrasena", usuario.Contrasena, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("v_id", dbType: OracleDbType.Int32, direction: ParameterDirection.ReturnValue);
+                    await connection.OpenAsync();
 
-                    await connection.ExecuteAsync(
-                        "pkg_usuarios.crear_usuario",
-                        parameters,
-                        commandType: CommandType.StoredProcedure
-                    );
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "pkg_usuarios.crear_usuario";
+                        command.CommandType = CommandType.StoredProcedure;
 
-                    int idGenerado = parameters.Get<int>("v_id");
+                        // Parámetros de entrada
+                        command.Parameters.Add("p_id_direccion", OracleDbType.Int32).Value =
+                            usuario.DireccionId.HasValue ? (object)usuario.DireccionId.Value : DBNull.Value;
+                        command.Parameters.Add("p_cedula", OracleDbType.Varchar2).Value = usuario.Cedula;
+                        command.Parameters.Add("p_primer_nombre", OracleDbType.Varchar2).Value = usuario.PrimerNombre;
+                        command.Parameters.Add("p_segundo_nombre", OracleDbType.Varchar2).Value =
+                            usuario.SegundoNombre ?? (object)DBNull.Value;
+                        command.Parameters.Add("p_apellido_paterno", OracleDbType.Varchar2).Value = usuario.ApellidoPaterno;
+                        command.Parameters.Add("p_apellido_materno", OracleDbType.Varchar2).Value =
+                            usuario.ApellidoMaterno ?? (object)DBNull.Value;
+                        command.Parameters.Add("p_telefono_principal", OracleDbType.Varchar2).Value = usuario.TelefonoPrincipal;
+                        command.Parameters.Add("p_telefono_secundario", OracleDbType.Varchar2).Value =
+                            usuario.TelefonoSecundario ?? (object)DBNull.Value;
+                        command.Parameters.Add("p_correo", OracleDbType.Varchar2).Value = usuario.Correo;
+                        command.Parameters.Add("p_contrasena", OracleDbType.Varchar2).Value = usuario.Contrasena;
 
-                    return Response<int>.Done("Usuario creado exitosamente", idGenerado);
+                        // Parámetro de salida (ID generado)
+                        var idParam = new OracleParameter("p_id_generado", OracleDbType.Int32, ParameterDirection.Output);
+                        command.Parameters.Add(idParam);
+
+                        await command.ExecuteNonQueryAsync();
+
+                        int idGenerado = ((OracleDecimal)idParam.Value).ToInt32();
+
+                        return Response<int>.Done("Usuario creado exitosamente", idGenerado);
+                    }
                 }
             }
             catch (OracleException ex)
             {
-                // Capturar errores específicos del paquete
                 if (ex.Message.Contains("ORA-20002"))
                 {
                     return Response<int>.Fail("El correo o la cédula ya existen");
@@ -109,32 +144,43 @@ namespace DAL.Implementaciones
             }
         }
 
+        // ========================================
+        // 3. ACTUALIZAR USUARIO
+        // ========================================
         public async Task<Response<bool>> ActualizarUsuario(UsuarioDTO usuario)
         {
             try
             {
                 using (var connection = new OracleConnection(_connectionString))
                 {
-                    var parameters = new OracleDynamicParameters();
-                    parameters.Add("p_id_usuario", usuario.Id, OracleDbType.Int32, ParameterDirection.Input);
-                    parameters.Add("p_id_direccion", usuario.DireccionId, OracleDbType.Int32, ParameterDirection.Input);
-                    parameters.Add("p_cedula", usuario.Cedula, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_primer_nombre", usuario.PrimerNombre, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_segundo_nombre", usuario.SegundoNombre, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_apellido_paterno", usuario.ApellidoPaterno, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_apellido_materno", usuario.ApellidoMaterno, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_telefono_principal", usuario.TelefonoPrincipal, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_telefono_secundario", usuario.TelefonoSecundario, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_correo", usuario.Correo, OracleDbType.Varchar2, ParameterDirection.Input);
-                    parameters.Add("p_contrasena", usuario.Contrasena, OracleDbType.Varchar2, ParameterDirection.Input);
+                    await connection.OpenAsync();
 
-                    await connection.ExecuteAsync(
-                        "pkg_usuarios.actualizar_usuario",
-                        parameters,
-                        commandType: CommandType.StoredProcedure
-                    );
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "pkg_usuarios.actualizar_usuario";
+                        command.CommandType = CommandType.StoredProcedure;
 
-                    return Response<bool>.Done("Usuario actualizado exitosamente", true);
+                        // Parámetros
+                        command.Parameters.Add("p_id_usuario", OracleDbType.Int32).Value = usuario.Id;
+                        command.Parameters.Add("p_id_direccion", OracleDbType.Int32).Value =
+                            usuario.DireccionId.HasValue ? (object)usuario.DireccionId.Value : DBNull.Value;
+                        command.Parameters.Add("p_cedula", OracleDbType.Varchar2).Value = usuario.Cedula;
+                        command.Parameters.Add("p_primer_nombre", OracleDbType.Varchar2).Value = usuario.PrimerNombre;
+                        command.Parameters.Add("p_segundo_nombre", OracleDbType.Varchar2).Value =
+                            usuario.SegundoNombre ?? (object)DBNull.Value;
+                        command.Parameters.Add("p_apellido_paterno", OracleDbType.Varchar2).Value = usuario.ApellidoPaterno;
+                        command.Parameters.Add("p_apellido_materno", OracleDbType.Varchar2).Value =
+                            usuario.ApellidoMaterno ?? (object)DBNull.Value;
+                        command.Parameters.Add("p_telefono_principal", OracleDbType.Varchar2).Value = usuario.TelefonoPrincipal;
+                        command.Parameters.Add("p_telefono_secundario", OracleDbType.Varchar2).Value =
+                            usuario.TelefonoSecundario ?? (object)DBNull.Value;
+                        command.Parameters.Add("p_correo", OracleDbType.Varchar2).Value = usuario.Correo;
+                        command.Parameters.Add("p_contrasena", OracleDbType.Varchar2).Value = usuario.Contrasena;
+
+                        await command.ExecuteNonQueryAsync();
+
+                        return Response<bool>.Done("Usuario actualizado exitosamente", true);
+                    }
                 }
             }
             catch (OracleException ex)
@@ -155,33 +201,51 @@ namespace DAL.Implementaciones
             }
         }
 
+        // ========================================
+        // 4. OBTENER USUARIOS
+        // ========================================
         public async Task<Response<UsuarioDTO>> ObtenerUsuarios()
         {
             try
             {
                 using (var connection = new OracleConnection(_connectionString))
                 {
-                    var parameters = new OracleDynamicParameters();
-                    parameters.Add("v_cursor", dbType: OracleDbType.RefCursor, direction: ParameterDirection.ReturnValue);
+                    await connection.OpenAsync();
 
-                    var usuarios = await connection.QueryAsync<UsuarioListaDTO>(
-                        "pkg_usuarios.listar_usuarios_activos",
-                        parameters,
-                        commandType: CommandType.StoredProcedure
-                    );
-
-                    // Mapear a UsuarioDTO
-                    var usuariosDTO = usuarios.Select(u => new UsuarioDTO
+                    using (var command = connection.CreateCommand())
                     {
-                        Id = u.IdUsuario,
-                        PrimerNombre = u.PrimerNombre,
-                        ApellidoPaterno = u.ApellidoPaterno,
-                        Correo = u.Correo,
-                        TelefonoPrincipal = u.TelefonoPrincipal,
-                        FechaRegistro = u.FechaRegistro
-                    }).ToList();
+                        command.CommandText = "pkg_usuarios.listar_usuarios_activos";
+                        command.CommandType = CommandType.StoredProcedure;
 
-                    return Response<UsuarioDTO>.Done("Usuarios obtenidos exitosamente", list: usuariosDTO);
+                        // Parámetro de salida (cursor)
+                        var cursorParam = new OracleParameter("p_cursor", OracleDbType.RefCursor, ParameterDirection.Output);
+                        command.Parameters.Add(cursorParam);
+
+                        await command.ExecuteNonQueryAsync();
+
+                        var usuarios = new List<UsuarioDTO>();
+
+                        // Leer el cursor
+                        using (var reader = ((OracleRefCursor)cursorParam.Value).GetDataReader())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                var usuario = new UsuarioDTO
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ID_USUARIO")),
+                                    PrimerNombre = reader.GetString(reader.GetOrdinal("PRIMER_NOMBRE")),
+                                    ApellidoPaterno = reader.GetString(reader.GetOrdinal("APELLIDO_PATERNO")),
+                                    Correo = reader.GetString(reader.GetOrdinal("CORREO")),
+                                    TelefonoPrincipal = reader.GetString(reader.GetOrdinal("TELEFONO_PRINCIPAL")),
+                                    FechaRegistro = reader.GetDateTime(reader.GetOrdinal("FECHA_REGISTRO"))
+                                };
+
+                                usuarios.Add(usuario);
+                            }
+                        }
+
+                        return Response<UsuarioDTO>.Done("Usuarios obtenidos exitosamente", list: usuarios);
+                    }
                 }
             }
             catch (Exception ex)
@@ -190,22 +254,29 @@ namespace DAL.Implementaciones
             }
         }
 
+        // ========================================
+        // 5. ELIMINAR USUARIO
+        // ========================================
         public async Task<Response<bool>> EliminarUsuario(int idUsuario)
         {
             try
             {
                 using (var connection = new OracleConnection(_connectionString))
                 {
-                    var parameters = new OracleDynamicParameters();
-                    parameters.Add("p_id_usuario", idUsuario, OracleDbType.Int32, ParameterDirection.Input);
+                    await connection.OpenAsync();
 
-                    await connection.ExecuteAsync(
-                        "pkg_usuarios.eliminar_usuario",
-                        parameters,
-                        commandType: CommandType.StoredProcedure
-                    );
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "pkg_usuarios.eliminar_usuario";
+                        command.CommandType = CommandType.StoredProcedure;
 
-                    return Response<bool>.Done("Usuario eliminado exitosamente", true);
+                        // Parámetro
+                        command.Parameters.Add("p_id_usuario", OracleDbType.Int32).Value = idUsuario;
+
+                        await command.ExecuteNonQueryAsync();
+
+                        return Response<bool>.Done("Usuario eliminado exitosamente", true);
+                    }
                 }
             }
             catch (OracleException ex)
--- DAL/Utilidades/OracleDbContext.cs
+++ DAL/Utilidades/OracleDbContext.cs
@@ -1,7 +1,12 @@
 ﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DATA
+namespace DAL.Utilidades
 {
     public class OracleDbContext
     {
